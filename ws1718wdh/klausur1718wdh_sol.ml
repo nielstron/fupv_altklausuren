5fe75d6f551fa0468626e8049227a220a61fe991
Niels Mündler <n.muendler@tum.de>
2019-02-20 00:28:48 +0100

    Add solution proposals for 1718wdh

--- ws1718wdh/klausur1718wdh_sol.ml
+++ ws1718wdh/klausur1718wdh_sol.ml
@@ -1,3 +1,6 @@
+open Thread
+open Event
+
 (*let _ = let rec f x = f x in f 1*)
 
 let _ = fun f b a -> f (a,b) 
@@ -27,6 +30,86 @@ module Peano = struct
   let leq p1 p2 = if p1 = p2 then true else match sub p2 p1 with 
       | Z -> false
       | S _ -> true
+end
+
+let find1 s l = let rec helper s l =
+                  match l with (sq, r)::xs -> if sq = s then ((Some r), xs) else 
+                      let (fr, xs) = helper s xs in (fr, ((sq, r)::xs))
+                             | [] -> (None, [])
+  in let (res, xs) = helper s l in match res with None -> (None, xs)
+                                                | Some r -> (res, (s,r)::xs)
+
+module MFU = struct
+  let rec init l = match l with (sq, r)::xs -> (0, (sq, r))::(init xs)
+                              | [] -> []
+  let find2 s l = let rec helper s l =
+                    match l with 
+                    | (f, (sq, r))::xs -> if sq = s then (Some (f+1, (sq, r)), xs) else 
+                        let (res, xs) = helper s xs in
+                        ( match res with None -> (None, (f, (sq, r))::xs)
+                                       | Some (f2, (sq2, r2)) -> if f2 >= f then (res, (f, (sq, r))::xs) else
+                                           (res, (f, (sq, r))::(f2, (sq2, r2))::xs))
+                    | [] -> (None, [])
+    in let (res, xs) = helper s l in match res with None -> (None, xs)
+                                                  | Some (f, (s, r)) -> (Some r, xs)
+
+end
+
+module type S = sig
+  type t
+  val size : t -> int
+  val show : t -> string
+end
+
+module SPair (A:S) (B:S) = struct
+  type t = A.t * B.t
+  let size (a, b) = A.size a + B.size b
+  let show (a, b) = "(" ^ A.show a ^ ", " ^ B.show b ^ ")"
+end
+
+module SList (A:S) = struct
+  type t = A.t list
+  let rec size a = match a with x::xs -> A.size x + size xs
+                              | [] -> 0
+  let show a = let rec helper a = match a with 
+      | x::[] -> A.show x
+      | x::xs -> A.show x ^ "; "
+      | [] -> ""
+    in "[ " ^ helper a ^ "]"
+end
+
+module SEither (At:S) (Bt:S) = struct
+  type t = A of At.t | B of Bt.t
+  let rec size ab = match ab with A a -> At.size a
+                                | B b -> Bt.size b
+  let rec show ab = match ab with A a -> At.show a
+                                | B b -> Bt.show b
+end
+
+module P2P = struct
+  type ('a, 'b) t = Publish of 'a * 'b channel
+                  | Request of 'a * 'b event option channel
+  let broker () = let ch = new_channel () in
+    let rec serve v = 
+      match sync (receive ch) with 
+      | Publish (a, b) -> serve ((a, b)::v)
+      | Request (a, b) -> 
+        let data_channel_opt = List.assoc_opt a v in
+        match data_channel_opt with None -> 
+          let _ = create (fun () -> sync (send b (None))) ()
+          in serve v
+                                  | Some dc -> let _ = create (fun () -> sync (send b (Some (receive dc))))
+                                    in serve v
+    in serve []
+
+  let publish broker key v = let ch = new_channel () in
+    let rec publish () = sync (send ch v); publish ()
+    in let _ = create publish ()
+    in sync (send broker (Publish (key, ch)))
+  let request broker key = let ch = new_channel () in
+    sync (send broker (Request (key, ch))); match sync (receive ch) with
+    | None -> None
+    | Some repch -> sync (repch)
 end
 
 (* ---- Test environment ---- *)
